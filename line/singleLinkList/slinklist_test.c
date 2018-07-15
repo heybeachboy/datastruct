4c5b6bc0f300d89737d2b277bcc0087e08574ce2
zhouletianvip6 <zhouletian1234@live.com>
2018-07-15 19:24:56 +0800

    add test file

--- line/singleLinkList/slinklist_test.c
+++ line/singleLinkList/slinklist_test.c
@@ -5,12 +5,17 @@
 int main() {
     Node h;	
     int arr[4] = {11111,22,33,44};
-    int *v1 = 3333;
-    int *v2 = 566;    
+    int v1 = 3333;
+    int v2 = 566;
+    char v3[] = "Hello! girl!!!\n";    
     init_single_link(&arr[3]);
     h = slink_get_first();
-    slink_insert(2,v1);
-    slink_insert(3,v2);
-    h = slink_get_index(3);
-    printf("this first value:%d\n",h->val);
+    slink_insert(2,&v1);
+    slink_insert(3,&v2);
+    slink_insert(4,&v3);
+    h = slink_get_index(4);
+    slink_insert(5,&v3);
+    printf("this first value:%s\n",h->val);
+    printf("single link size: %d\n",slink_size());
+    free_single_link();
 }    
